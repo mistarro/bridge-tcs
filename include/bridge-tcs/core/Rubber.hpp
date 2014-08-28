ed0f6976a14e0e4de7b5fe8b3a74fd95e08dd175
Michał Staromiejski <michal.staromiejski@gmail.com>
2014-08-28 15:25:10 +0200

    Added empty class core::Rubber

---
+++ include/bridge-tcs/core/Rubber.hpp
@@ -0,0 +1,14 @@
+#ifndef BRIDGETCS_CORE_RUBBER_HPP
+#define BRIDGETCS_CORE_RUBBER_HPP
+
+namespace bridge_tcs {
+namespace core {
+
+class Rubber
+{
+};
+
+} // namespace core
+} // namespace bridge_tcs
+
+#endif // BRIDGETCS_CORE_RUBBER_HPP
